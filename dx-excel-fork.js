3563e3dd7cb1fdb25faadae43d9e8fcc76e99b9e
Eldar Iusupzhanov <ferrarijed@gmail.com>
2025-08-01 13:18:18 +0800

    change comment

--- dx-excel-fork.js
+++ dx-excel-fork.js
@@ -1,12 +1,9 @@
-/*!
- * DevExtreme-ExcelJS Fork
- * https://js.devexpress.com/
+/**
  * Copyright (c) 2025, Developer Express Inc.
  * Copyright (c) 2014-2019 Guyon Roche
  * LICENCE: MIT - please refer to LICENSE file included with this module
  * or https://github.com/DevExpress/devextreme-exceljs-fork/blob/master/LICENSE
  */
-
 if (parseInt(process.versions.node.split('.')[0], 10) < 10) {
   throw new Error(
     'For node versions older than 10, please use the ES5 Import: https://github.com/exceljs/exceljs#es5-imports'
