236d83f6fc3f25311214207de014600f75560687
Eldar Iusupzhanov <84278206+Tucchhaa@users.noreply.github.com>
2026-04-29 18:00:18 +0800

    Bump package version to 4.4.9 (#59)

--- spec/unit/xlsx/xform/sheet/cf-ext/cf-rule-ext-xform.spec.js
+++ spec/unit/xlsx/xform/sheet/cf-ext/cf-rule-ext-xform.spec.js
@@ -1,9 +1,6 @@
 const testXformHelper = require('../../test-xform-helper');
 
-const XmlStream = verquire('utils/xml-stream');
 const CfRuleExtXform = verquire('xlsx/xform/sheet/cf-ext/cf-rule-ext-xform');
-
-const FAKE_UUID = '00000000-0000-0000-0000-000000000000';
 
 const expectations = [
   {
@@ -87,84 +84,8 @@ const expectations = [
     },
     tests: ['render', 'parse'],
   },
-  {
-    title: 'Icon Set - prepare assigns UUID',
-    create() {
-      return new CfRuleExtXform();
-    },
-    initialModel: {
-      type: 'iconSet',
-      priority: 1,
-      iconSet: '3Triangles',
-      cfvo: [
-        {type: 'percent', value: 0},
-        {type: 'percent', value: 33},
-        {type: 'percent', value: 67},
-      ],
-    },
-    xml: `
-      <x14:cfRule type="iconSet" priority="1" id="{${FAKE_UUID.toUpperCase()}}">
-        <x14:iconSet iconSet="3Triangles">
-          <x14:cfvo type="percent"><xm:f>0</xm:f></x14:cfvo>
-          <x14:cfvo type="percent"><xm:f>33</xm:f></x14:cfvo>
-          <x14:cfvo type="percent"><xm:f>67</xm:f></x14:cfvo>
-        </x14:iconSet>
-      </x14:cfRule>
-    `,
-    tests: ['prepare-render'],
-  },
-  {
-    title: 'DataBar - prepare assigns UUID',
-    create() {
-      return new CfRuleExtXform();
-    },
-    initialModel: {
-      type: 'dataBar',
-      cfvo: [
-        {type: 'num', value: 5},
-        {type: 'num', value: 20},
-      ],
-    },
-    xml: `
-      <x14:cfRule type="dataBar" id="{${FAKE_UUID.toUpperCase()}}">
-        <x14:dataBar minLength="0" maxLength="100">
-          <x14:cfvo type="num"><xm:f>5</xm:f></x14:cfvo>
-          <x14:cfvo type="num"><xm:f>20</xm:f></x14:cfvo>
-        </x14:dataBar>
-      </x14:cfRule>
-    `,
-    tests: ['prepare-render'],
-  },
 ];
 
 describe('CfRuleExtXform', () => {
-  let originalRandomUUID;
-
-  beforeEach(() => {
-    originalRandomUUID = globalThis.crypto.randomUUID;
-    globalThis.crypto.randomUUID = () => '00000000-0000-0000-0000-000000000000';
-  });
-
-  afterEach(() => {
-    globalThis.crypto.randomUUID = originalRandomUUID;
-  });
-
   testXformHelper(expectations);
-
-  it('renderIconSet uses fallback UUID when x14Id is missing', () => {
-    const xform = new CfRuleExtXform();
-    const xmlStream = new XmlStream();
-    xform.render(xmlStream, {
-      type: 'iconSet',
-      priority: 1,
-      iconSet: '3Triangles',
-      cfvo: [
-        {type: 'percent', value: 0},
-        {type: 'percent', value: 33},
-        {type: 'percent', value: 67},
-      ],
-    });
-
-    expect(xmlStream.xml).to.contain(`id="{${FAKE_UUID}}"`);
-  });
 });
