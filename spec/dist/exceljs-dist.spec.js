590410a26822723e3a1f28396449e59f90a08a89
Anna Shakhova <68295572+anna-shakhova@users.noreply.github.com>
2026-07-16 11:37:51 +0200

    migrate esling config for ESlint v10 (#129)

--- spec/dist/exceljs-dist.spec.js
+++ spec/dist/exceljs-dist.spec.js
@@ -1,26 +1,26 @@
 const fs = require('fs');
 
-const exists = path => new Promise(resolve => fs.exists(path, resolve));
+const exists = path => fs.promises.access(path).then(() => true).catch(() => false);
 
 describe('ExcelJS', () => {
   describe('dist folder', () => {
     it('should include LICENSE', async () => {
-      expect(await exists('./dist/LICENSE')).to.be.true()
+      expect(await exists('./dist/LICENSE')).to.be.true();
     });
     it('should include dx-exceljs-fork.js', async () => {
-      expect(await exists('./dist/dx-exceljs-fork.js')).to.be.true()
+      expect(await exists('./dist/dx-exceljs-fork.js')).to.be.true();
     });
     it('should include dx-exceljs-fork.min.js', async () => {
-      expect(await exists('./dist/dx-exceljs-fork.min.js')).to.be.true()
+      expect(await exists('./dist/dx-exceljs-fork.min.js')).to.be.true();
     });
     it('should include dx-exceljs-fork.bare.js', async () => {
-      expect(await exists('./dist/dx-exceljs-fork.bare.js')).to.be.true()
+      expect(await exists('./dist/dx-exceljs-fork.bare.js')).to.be.true();
     });
     it('should include dx-exceljs-fork.bare.min.js', async () => {
-      expect(await exists('./dist/dx-exceljs-fork.bare.min.js')).to.be.true()
+      expect(await exists('./dist/dx-exceljs-fork.bare.min.js')).to.be.true();
     });
     it('should include es5/index', async () => {
-      expect(await exists('./dist/es5/index.js')).to.be.true()
+      expect(await exists('./dist/es5/index.js')).to.be.true();
     });
   });
 });
