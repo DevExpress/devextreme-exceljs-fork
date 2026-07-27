93fa497ac54ff93d2a9c87ddf697ec7fe121961c
Maksim Zakharov <251575087+bit-byte0@users.noreply.github.com>
2026-07-27 17:55:40 +0400

    exceljs-fork: drop airbnb-base for eslint and migrate to eslint-plugin-import-x (#141)

--- spec/utils/verquire.js
+++ spec/utils/verquire.js
@@ -1,7 +1,7 @@
 // this module allows the specs to switch between source code and
 // transpiled code depending on the environment variable EXCEL_BUILD
 
-/* eslint-disable import/no-dynamic-require */
+/* eslint-disable import-x/no-dynamic-require */
 
 const libs = {};
 const basePath = (function() {
